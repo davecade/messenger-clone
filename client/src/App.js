97da1bc4d87569310c837492a68a266c86b43356
Dave <dave.cadelina@outlook.com>
2023-07-02 11:23:19 +1000

    started working on messenger component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Messenger from "./components/Messenger";
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 				<Routes>
 					<Route path="/messenger/login" element={<Login />} />
 					<Route path="/messenger/register" element={<Register />} />
+					<Route path="/" element={<Messenger />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
