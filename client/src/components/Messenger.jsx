c6078555cb564bfec0c1af6e7accf2311d4c41bd
Dave <dave.cadelina@outlook.com>
2023-07-04 00:58:48 +1000

    added styles to split sections

--- client/src/components/Messenger.jsx
+++ client/src/components/Messenger.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaEllipsisH, FaEdit, FaSistrix } from "react-icons/fa";
 
 const Messenger = () => {
+
 	return (
 		<div className="messenger">
 			<div className="row">
@@ -14,31 +15,39 @@ const Messenger = () => {
 										src="/image/28885Dave_._3D_light_background_cool_profile_picture_detailed_07085e90-d908-4ec8-8d95-fdf97ae1ed12.png"
 										alt=""
 									/>
-									<div className="name">
-										<h3>Hi Dave</h3>
-									</div>
-									<div className="icons">
-										<div className="icon">
-											<FaEllipsisH />
-										</div>
-										<div className="icon">
-											<FaEdit />
-										</div>
-									</div>
 								</div>
-								<div className="friend-search">
-									<div className="search">
-										<button>
-											<FaSistrix />
-										</button>
-										<input
-											type="text"
-											placeholder="Search"
-											className="form-control"
-										/>
-									</div>
+								<div className="name">
+									<h3> HI Dave </h3>
 								</div>
 							</div>
+
+							<div className="icons">
+								<div className="icon">
+									<FaEllipsisH />
+								</div>
+								<div className="icon">
+									<FaEdit />
+								</div>
+							</div>
+						</div>
+
+						<div className="friend-search">
+							<div className="search">
+								<button>
+									{" "}
+									<FaSistrix />{" "}
+								</button>
+								<input
+									type="text"
+									placeholder="Search"
+									className="form-control"
+								/>
+							</div>
+						</div>
+
+						<div className="active-friends"></div>
+						<div className="friends">
+							<div className="hover-friend"></div>
 						</div>
 					</div>
 				</div>
